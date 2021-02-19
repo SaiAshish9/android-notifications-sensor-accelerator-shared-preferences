99aa2d7d5ece241adcad3ad716e9fd29717b65d7
saiashish9 <saiashish3760@gmail.com>
2021-02-20 02:55:55 +0530

    completed

--- app/src/main/java/com/example/androidnotificationssharedpreferencessensoracc/MainActivity.java
+++ app/src/main/java/com/example/androidnotificationssharedpreferencessensoracc/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.androidnotificationssharedpreferencessensoracc;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    Sensor accelerometer;
+    SensorManager sm;
+    TextView  tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sm=(SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer=sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(this,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        tv= (TextView) findViewById(R.id.acc);
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        tv.setText("\nX: "+event.values[0]+"\nY: "+event.values[1]+"\nZ: "+event.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
